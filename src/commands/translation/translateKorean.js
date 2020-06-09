26a0b696064e4e430780a603afd9fdeef07327c0
J3sven <sven@nevs.me>
2020-06-09 22:30:14 +0200

    moved debg out of function

--- src/commands/translation/translateKorean.js
+++ src/commands/translation/translateKorean.js
@@ -2,9 +2,8 @@ const { translateKorean } = require('../../utils/translator');
 
 module.exports = {
     run: async(client, message, args) => {
-
+        message.channel.send("I am trying ok?");
         async function translationResults() {
-            message.channel.send("I am trying ok?");
             resultText = await translateKorean(args);
             message.channel.send(":flag_gb: " + resultText.text);
         }
